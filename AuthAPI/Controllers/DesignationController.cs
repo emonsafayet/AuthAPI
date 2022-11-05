be9c33b697fdcdc581a8afdc1eb320f659f59466
Safayet Hossain <emonsafayet@gmail.com>
2022-11-05 21:25:26 +0600

    .

--- AuthAPI/Controllers/DesignationController.cs
+++ AuthAPI/Controllers/DesignationController.cs
@@ -4,8 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AuthAPI.Models;
-using AuthAPI.Models;
+using AuthAPI.Models; 
 
 namespace CustomerAPI.Controllers
 {
